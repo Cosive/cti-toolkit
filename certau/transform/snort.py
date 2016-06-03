c890ddbd889239e71e17d9db0489bcab7e3676a7
Terry MacDonald <terry.macdonald@cosive.com>
2016-06-03 10:53:31 +1000

    Added Domain and URL support to Snort output
     Added Domain and URL support to Snort output
    Also updated the snort test so that it works

--- certau/transform/snort.py
+++ certau/transform/snort.py
@@ -2,6 +2,8 @@ import re
 
 from cybox.objects.address_object import Address
 from cybox.objects.uri_object import URI
+from urlparse import urlparse
+import pprint
 
 from .text import StixTextTransform
 
@@ -29,21 +31,30 @@ class StixSnortTransform(StixTextTransform):
     """
 
     OBJECT_FIELDS = {
-        'Address': ['address_value'],
+        'Address': ['category', 'address_value'],
         'DomainName': ['value'],
         'EmailMessage': [
             'header.from_.address_value',
             'header.to.address_value',
+            'header.subject',
+            'attachments.object_reference',
         ],
-        'File': ['hashes.simple_hash_value'],
+        'File': ['file_name', 'hashes.type_', 'hashes.simple_hash_value'],
         'HTTPSession': ['http_request_response.http_client_request.' +
                         'http_request_header.parsed_header.user_agent'],
-        'SocketAddress': ['ip_address.address_value'],
+        'Mutex': ['name'],
+        'SocketAddress': [
+            'ip_address.category',
+            'ip_address.address_value',
+            'port.port_value',
+            'port.layer4_protocol',
+        ],
         'URI': ['value'],
+        'WinRegistryKey': ['hive', 'key', 'values.name', 'values.data'],
     }
 
     def __init__(self, package, separator='\t', include_header=False,
-                 header_prefix='#', snort_initial_sid=5500000, snort_rule_revision=1, snort_rule_action="alert"):
+                 header_prefix='#', snort_initial_sid=5500000, snort_rule_revision=1, snort_rule_action="alert", ):
         super(StixSnortTransform, self).__init__(
             package, separator, include_header, header_prefix
         )
@@ -56,11 +67,21 @@ class StixSnortTransform(StixTextTransform):
         if object_type in self._observables:
             for observable in self._observables[object_type]:
                 id_ = observable['id']
-                try:
-                    for fields in observable['fields']:
-                        ip = fields["address_value"]
-                        text += '{} ip any any -> {} any (flow:established,to_server; msg:"CTI-TOOLKIT Connection to potentially malicious server {} (ID {})"; sid:{}; rev:{}; classtype:bad-unknown;)\n'.format(self._snort_rule_action, ip, ip, id_, self._sid, self._snort_rule_revision)
-                        self._sid += 1
-                except KeyError:
-                    pass
+                if self.OBJECT_FIELDS and object_type in self.OBJECT_FIELDS:
+                    if object_type == "Address":
+                        for field in observable['fields']:
+                            ip  = field["address_value"]
+                            text += '{} ip any any -> {} any (flow:established,to_server; msg:"CTI-TOOLKIT Connection to potentially malicious server {} (ID {})"; sid:{}; rev:{}; classtype:bad-unknown;)\n'.format(self._snort_rule_action, ip, ip, id_, self._sid, self._snort_rule_revision)
+                            self._sid += 1
+                    elif object_type == "URI":
+                        for field in observable['fields']:
+                            url  = urlparse(field["value"])
+                            text += '{} tcp any any -> $EXTERNAL_NET $HTTP_PORTS (flow:established,to_server; content:"{}"; http_header; nocase; uricontent:"{}"; nocase; msg:"CTI-TOOLKIT Connection to potentially malicious url {} (ID {})"; sid:{}; rev:{}; classtype:bad-unknown;)\n'.format(self._snort_rule_action, url.netloc, url.path, url.geturl(), id_, self._sid, self._snort_rule_revision)
+                            self._sid += 1
+                    elif object_type == "DomainName":
+                        for field in observable['fields']:
+                            domain  = field["value"]
+                            text += '{} tcp any any -> $EXTERNAL_NET $HTTP_PORTS (flow:established,to_server; content:"{}"; http_header; nocase; msg:"CTI-TOOLKIT Connection to potentially malicious domain {} (ID {})"; sid:{}; rev:{}; classtype:bad-unknown;)\n'.format(self._snort_rule_action, domain, domain, id_, self._sid, self._snort_rule_revision)
+                            self._sid += 1
+
         return text
