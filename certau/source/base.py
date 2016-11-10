8a2ecab72874a834081a320a7145efad61ed33e3
terrymacdonald <terry.macdonald@cosive.com>
2016-11-10 17:03:52 +1300

    Added more Exception handling and error messages to stix import
    Added more exception handling and error messages to the stix source
    base.py file to better handle exceptions.

--- certau/source/base.py
+++ certau/source/base.py
@@ -9,18 +9,27 @@ class StixSource(object):
     def load_stix_package(self, stix_file, stix_version='1.2'):
         """Helper for loading and updating (if required) a STIX package."""
         try:
-            # TODO add in a version check to make sure that the returned STIX is
-            # what the user requested. If not, it needs ramrodding
+            # This will attempt to ingest the STIX package
+            # if its not at a version supported by the version of python-stix, then we try to upgrade it
             package = STIXPackage.from_xml(stix_file)
             
-        except UnsupportedVersionError:
-            updated = ramrod.update(stix_file, to_=stix_version)
-            document = updated.document.as_stringio()
+        except UnsupportedVersionError as e:
+            print "Found STIX Version {0}. Converting to {0}: {}".format(e.found, e.expected, e.message)
             try:
+                updated = ramrod.update(stix_file, to_=stix_version)
+                document = updated.document.as_stringio()
                 package = STIXPackage.from_xml(document)
-            except Exception:
+            except UnsupportedVersionError as e:
+                print "ERROR - STIX Version {0} is unsupported: {}".format(e.found, e.message)
                 package = None
-        except Exception:
+            except Exception as e:
+                print "ERROR - Exception occurred: {}".format(e.message)
+                package = None
+        except UnknownVersionError:
+            print "ERROR - No STIX Version found in the source document."
+            package = None
+        except Exception as e:
+            print "ERROR - Exception occurred: {}".format(e.message)
             package = None
 
         return package
