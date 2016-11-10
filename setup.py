5d686461b6f4b929dcad80bb5542b5206be14303
terrymacdonald <terry.macdonald@cosive.com>
2016-11-11 12:18:57 +1300

    Merge fixes to setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,11 @@
 
 from setuptools import setup
 
-<<<<<<< HEAD
 setup(
     name='cti-toolkit',
     version='1.1.0.dev2',
     description='CERT Australia cyber threat intelligence (CTI) toolkit',
     url='https://github.com/certau/cti-toolkit/',
-=======
-setup(name='certau_stix_toolkit',
-    version='1.0.1',
-    description='CERT Australia STIX utilities',
->>>>>>> refs/remotes/origin/stix-1.2
     author='CERT Australia, Australian Government',
     author_email='info@cert.gov.au',
     license='BSD',
@@ -43,15 +37,9 @@ setup(name='certau_stix_toolkit',
         'configargparse',
         'lxml',
         'libtaxii',
-<<<<<<< HEAD
-        'cybox==2.1.0.12',
-        'stix==1.1.1.7',
-        'stix-ramrod',
-=======
         'cybox==2.1.0.13',
         'stix==1.2.0.2',
         'stix-ramrod==1.1.0',
->>>>>>> refs/remotes/origin/stix-1.2
         'mixbox',
         'pymisp',
         'requests',
