7aba47e4ea5acc336d5e8880950310e9aca65b8b
kralca <kralca@gmail.com>
2017-07-17 18:36:23 +1000

    Use built-in string conversion for helpers.

--- certau/lib/stix/helpers.py
+++ certau/lib/stix/helpers.py
@@ -5,14 +5,14 @@ from stix.extensions.marking.tlp import TLPMarkingStructure
 def package_title(package):
     """Retrieves the STIX package title (str) from the header."""
     if package.stix_header and package.stix_header.title:
-        return package.stix_header.title.encode('utf-8')
+        return str(package.stix_header.title)
     else:
         return None
 
 def package_description(package):
     """Retrieves the STIX package description (str) from the header."""
     if package.stix_header and package.stix_header.description:
-        return package.stix_header.description.value.encode('utf-8')
+        return str(package.stix_header.description)
     else:
         return None
 
@@ -24,5 +24,5 @@ def package_tlp(package):
             for marking_spec in handling.marking:
                 for marking_struct in marking_spec.marking_structures:
                     if isinstance(marking_struct, TLPMarkingStructure):
-                        return marking_struct.color
+                        return str(marking_struct.color)
     return None
